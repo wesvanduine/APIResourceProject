1a9d14da5ad05e68670c3fa771a8ae21c4856b4c
ptk88 <phillipklemm@gmail.com>
2017-05-25 20:28:41 -0700

    added search functionality

---
+++ js/template.js
@@ -0,0 +1,13 @@
+
+return `
+<div CLASS="thumbnail">
+    <img src="${img}" alt="">
+    <div CLASS="caption">
+        <h3>${title}</h3>
+        <p>${description}</p>
+        <p>
+            <a href="#" CLASS="btn btn-primary">Buy Now!</a> <a href="#" CLASS="btn btn-default">More Info</a>
+        </p>
+    </div>
+</div>
+`
